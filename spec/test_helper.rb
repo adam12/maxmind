aa9d0a0a7bcc977b9e626d9a3a8a97622eb2ed3e
Tom Blomfield <tomblomfield@gmail.com>
2012-08-14 16:45:05 +0100

    gemfile

---
+++ spec/test_helper.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+require 'matchy'
+require 'fakeweb'
+
+FakeWeb.allow_net_connect = false
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'maxmind'
+
+class Test::Unit::TestCase
+end

e0fa107689aaef09fdf8bfb1d2fc6fdc9dfe4334
Adam Daniels <adam@mediadrive.ca>
2013-01-20 01:08:33 -0500

    Remove ActiveSupport dependency

--- lib/maxmind.rb
+++ lib/maxmind.rb
@@ -1,9 +1,3 @@
-require 'active_support'
-unless Module.respond_to?(:mattr_accessor)
-  require 'active_support/core_ext/module/attribute_accessors'# rescue nil  # this may be needed for ActiveSupport versions >= 3.x
-end
-require 'active_support/core_ext/hash'
-require 'net/http'
 require 'net/https'
 require 'uri'
 require 'digest/md5'
--- maxmind.gemspec
+++ maxmind.gemspec
@@ -24,7 +24,4 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rspec'
   s.add_development_dependency 'mocha'
   s.add_development_dependency 'webmock'
-
-  s.add_runtime_dependency 'activesupport', '>= 3.0.0'
 end
-

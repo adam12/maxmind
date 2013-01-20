e5533d06d5f3ffc52b2138534ed4a786d00014bc
Adam Daniels <adam@mediadrive.ca>
2013-01-20 00:31:33 -0500

    Change require's to relative paths

--- lib/maxmind.rb
+++ lib/maxmind.rb
@@ -7,5 +7,7 @@ require 'net/http'
 require 'net/https'
 require 'uri'
 require 'digest/md5'
-require File.join(File.dirname(__FILE__), 'maxmind/request')
-require File.join(File.dirname(__FILE__), 'maxmind/response')
+
+require 'maxmind/version'
+require 'maxmind/request'
+require 'maxmind/response'
